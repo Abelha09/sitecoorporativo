042d5ceea6770358873f68b489d1a3465aad600f
Bárbara Abelha <146784507+Abelha09@users.noreply.github.com>
2025-08-14 19:19:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // Tema light/dark
 const themeBtn = document.getElementById('theme-switch');
 themeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    document.body.classList.toggle('light');
+    if(document.body.classList.contains('light')) {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+        themeBtn.innerText = '☀️';
+    } else {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+        themeBtn.innerText = '🌙';
+    }
 });
 
 // Switch de idioma
@@ -10,7 +17,10 @@ const langSelect = document.getElementById('lang-switch');
 langSelect.addEventListener('change', () => {
     const lang = langSelect.value;
     document.querySelectorAll('[data-pt]').forEach(el => {
-        el.innerText = el.dataset[lang];
-        if(el.placeholder) el.placeholder = el.dataset[lang];
+        if(el.placeholder !== undefined) {
+            el.placeholder = el.dataset[lang];
+        } else {
+            el.innerText = el.dataset[lang];
+        }
     });
 });
